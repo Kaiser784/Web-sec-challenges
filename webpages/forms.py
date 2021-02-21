c1d8cc5f4861505f86c57f3bed8e3cfbceb49d9e
Binesh <bineshmunukurthi@gmail.com>
2021-02-21 12:05:45 +0530

    added staticfiles

--- webpages/forms.py
+++ webpages/forms.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class InputForm(UserCreationForm):
-    firstname = forms.CharField(max_length=25)
-    lastname = forms.CharField(max_length=25)
+    first_name = forms.CharField(max_length=25)
+    last_name = forms.CharField(max_length=25)
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ('firstname', 'lastname', 'email', 'username', 'password1', 'password2')
+        fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
