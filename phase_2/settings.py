7f188788abd0eb0778c8decd34544add301d5074
Binesh <bineshmunukurthi@gmail.com>
2021-02-21 13:03:51 +0530

    minor change

--- phase_2/settings.py
+++ phase_2/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECTRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['anfang.herokuapp.com']
+ALLOWED_HOSTS = ['anfang.herokuapp.com', 'localhost']
 
 
 # Application definition
