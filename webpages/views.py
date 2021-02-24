444dc628e3109ffc9332afb325b90ddb826d9291
unknown <abhi10012001@gmail.com>
2021-02-24 20:11:19 +0530

    login update

--- webpages/views.py
+++ webpages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from .forms import InputForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 
@@ -17,4 +18,23 @@ def register(request):
             print(form.errors)
     else:
         form = InputForm()
+    return render(request, 'register/register.html', {'form': form})
+
+def login(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are logged in as {username}")
+                return render(request, 'register/success.html', {'data': username})
+            else:
+                messages.error(request, "Invalid username or password")
+        else:
+            messages.error(request, "Invalid username or password")
+    form = AuthenticationForm()
     return render(request, 'register/login.html', {'form': form})
