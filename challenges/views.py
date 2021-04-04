c1e9c0fd700732b47f03fb01eb2ab6c718c75ab5
unknown <abhi10012001@gmail.com>
2021-04-04 19:43:43 +0530

    Added flag authentication and leaderboard updates

--- challenges/views.py
+++ challenges/views.py
@@ -1,21 +1,50 @@
 from django import forms
+from django.urls import reverse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.db.models import Q
-from .models import challenges_table, LeaderBoard
-
-
-class SubmitFlag(forms.Form):
-	Flag = forms.CharField(label="Flag                       :")
+from .models import ChallengeUserRelation, challenges_table, LeaderBoard
+from webpages.views import register
 
 # Create your views here.
 def home(request):
-	challenges = challenges_table.objects.all()
+	record1 = challenges_table.objects.all()
 
-	context = {
-		'challenges': challenges,
-	}
-	return render(request,'challenges/home.html', context)
+	if request.user.is_authenticated:
+		current_user = request.user
+		record2 = LeaderBoard.objects.get(user = current_user)
+		record3 = ChallengeUserRelation.objects.filter(user = current_user)
+
+		query_set = ChallengeUserRelation.objects.select_related('challenge').filter(user = current_user)
+
+		context = {
+			'challenges': query_set,	
+			'leaderboard': record2,
+		}
+		
+		var = request.POST
+		flagcheck = var.get("flag_check", False)
+		titlecheck = var.get("title_check", False)
+		if flagcheck:
+			record1 = challenges_table.objects.get(title = titlecheck)
+			if record1.flag == flagcheck:
+				print("YES")
+				record2.venari = record2.venari + record1.venari
+				record2.solves = record2.solves + 1
+				record2.save()
+				for record in record3:
+					if str(record.challenge) == str(titlecheck):
+						record.done = True
+						record.save()
+			else:
+				print("NO")
+			return HttpResponseRedirect(reverse('webpages-home'))
+
+		return render(request,'challenges/home.html', context)
+
+	else:
+		return register(request)
 
 def leaderboard(request):
 	context = {}
